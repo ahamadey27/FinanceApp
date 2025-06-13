da0c2abc6ae735d00efb78a73ed9f2e057b34d2e
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-13 15:53:05 -0400

    added NeGet references to prject

---
+++ FinanceApp/FinanceApp/Migrations/20250613195141_AddDateToExpense.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateToExpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Expenses",
+                newName: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Expenses",
+                newName: "MyProperty");
+        }
+    }
+}
