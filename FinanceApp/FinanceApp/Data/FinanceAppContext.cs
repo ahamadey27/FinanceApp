4f72f0cfd0c8245b6b8483e65a398c932ea2ef56
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-13 11:54:31 -0400

    getting sql sorted

--- FinanceApp/FinanceApp/Data/FinanceAppContext.cs
+++ FinanceApp/FinanceApp/Data/FinanceAppContext.cs
@@ -1,7 +1,16 @@
-﻿namespace FinanceApp.Data
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
 {
-    public class FinanceAppContext
+    public class FinanceAppContext : DbContext
     {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options)
+        {
+
+        }
+        //Click "View" up top "Server Explorer" and create database server and right click top to create data base
+        DbSet<Expense> Expenses { get; set; }
 
     }
 }
