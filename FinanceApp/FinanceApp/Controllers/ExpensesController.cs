af7fcc57c9a7f66b52c792c073aa739cb9793e10
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-13 15:29:42 -0400

    More contoller

--- FinanceApp/FinanceApp/Controllers/ExpensesController.cs
+++ FinanceApp/FinanceApp/Controllers/ExpensesController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceApp.Controllers
 {
     public class ExpensesController : Controller
     {
+        //Able to interact with database
+        private readonly FinanceAppContext _context;
+        public ExpensesController(FinanceAppContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
