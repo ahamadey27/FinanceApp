fd307da3aed87a2d77f3ed17cfb4ab0eaecaf2d1
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-13 11:27:42 -0400

    adding Model and data

---
+++ FinanceApp/FinanceApp/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        //Required is default
+        public int Id { get; set; }
+        [Required]
+        public string Description { get; set; } = null!;
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount needs to be more than zero")]
+        public double Amount { get; set; }
+        // Ensure the amount is greater than zero
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount needs to be more than zero")]
+        [Required]
+        public string Catagory { get; set; } = null!;
+        public DateTime MyProperty { get; set; } = DateTime.Now; 
+    }
+}
