968d765b1961d537766115b4d33173f95f1cce49
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-16 11:11:52 -0400

    Debugging and building new migraation

--- FinanceApp/FinanceApp/Models/Expense.cs
+++ FinanceApp/FinanceApp/Models/Expense.cs
@@ -1,20 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FinanceApp.Models
 {
     public class Expense
     {
-        //Required is default
         public int Id { get; set; }
+
         [Required]
         public string Description { get; set; } = null!;
+
         [Required]
-        [Range(0.01, double.MaxValue, ErrorMessage = "Need to add description")]
-        public double Amount { get; set; }
-        // Ensure the amount is greater than zero
         [Range(0.01, double.MaxValue, ErrorMessage = "Amount needs to be more than zero")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+
         [Required]
         public string Catagory { get; set; } = null!;
-        public DateTime Date { get; set; } = DateTime.Now; 
+
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
